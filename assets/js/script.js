996f06c8b43ab3b3fa75e55713ed9908f887cb7a
jtschams <jtschams@yahoo.com>
2024-04-10 22:51:19 -0500

    added modal box for adding tasks.  added functions to create task objects

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,13 @@ let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
-function generateTaskId() {
-
+function generateTaskId(task) {
+    if (!nextId) {
+        nextId = 0;
+    }
+    $(task).prop('id', nextId);
+    nextId++;
+    localStorage.setItem("nextId", nextId)
 }
 
 // Todo: create a function to create a task card
@@ -19,11 +24,24 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
-
+    event.preventDefault();
+    const task = {
+        title: $('#formTitle').val(),
+        dueDate: $('#formDate').val(),
+        description: $('#formDescription').val()
+    }
+    generateTaskId(task);
+    createTaskCard(task);
+    if (!taskList) {
+        taskList = [];
+    }
+    taskList.push(task);
+    localStorage.setItem("tasks", taskList)
 }
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
+    event.preventDefault();
 
 }
 
@@ -36,3 +54,5 @@ function handleDrop(event, ui) {
 $(document).ready(function () {
 
 });
+
+$('#addTask').on('submit', handleAddTask);
