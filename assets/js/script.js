3cf15aa8806554083a587d8155c69c8a5f6ca97b
jtschams <jtschams@yahoo.com>
2024-04-10 23:50:57 -0500

    added functions to create and render todo list cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,57 +2,73 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
-// Todo: create a function to generate a unique task id
+// Generate a unique task id
 function generateTaskId(task) {
     if (!nextId) {
         nextId = 0;
     }
     $(task).prop('id', nextId);
     nextId++;
-    localStorage.setItem("nextId", nextId)
+    localStorage.setItem("nextId", JSON.stringify(nextId));
 }
 
-// Todo: create a function to create a task card
+// Create a task card
 function createTaskCard(task) {
-
+    $(`#${task.todoStatus}-cards`).append(`<div class="card">
+    <div class="card-header bg-white">
+      <h2 class="card-title mb-1">${task.title}</h2>
+    </div>
+    <div class="card-body bg-light">
+      <div class="m-2">${task.description}</div>
+      <div class="m-2">${task.dueDate}</div>
+      <button type="button" class="btn btn-primary btn-danger">Delete</button>
+    </div>
+  </div>`)
 }
 
-// Todo: create a function to render the task list and make cards draggable
+// Render the task list
 function renderTaskList() {
-
+    for (task of taskList) {
+        createTaskCard(task);
+    }
 }
 
-// Todo: create a function to handle adding a new task
+// Handle adding a new task
 function handleAddTask(event){
     event.preventDefault();
     const task = {
         title: $('#formTitle').val(),
         dueDate: $('#formDate').val(),
-        description: $('#formDescription').val()
+        description: $('#formDescription').val(),
+        todoStatus: 'todo'
     }
+    $('.form-control').val('');
     generateTaskId(task);
     createTaskCard(task);
     if (!taskList) {
         taskList = [];
     }
     taskList.push(task);
-    localStorage.setItem("tasks", taskList)
+    localStorage.setItem("tasks", JSON.stringify(taskList));
 }
 
-// Todo: create a function to handle deleting a task
+//!!! Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
-    event.preventDefault();
 
 }
 
-// Todo: create a function to handle dropping a task into a new status lane
+//!!! Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
 
 }
 
-// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+//!!! Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    renderTaskList();
+    $( function() {
+        $( "#todo-cards, #in-progress-cards, #done-cards" ).sortable({
+            connectWith: ".listedTask"
+        }).disableSelection();
+    } );
+    $('#addTask').on('submit', handleAddTask);
 });
-
-$('#addTask').on('submit', handleAddTask);
