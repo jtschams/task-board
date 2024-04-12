04a498b4023d425b79b2014f25718d30043e0f84
jtschams <jtschams@yahoo.com>
2024-04-11 22:40:34 -0500

    added dragging and sorting to task cards. added color coding to task cards. added datepicker widget.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,6 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Generate a unique task id
 function generateTaskId(task) {
-    if (!nextId) {
-        nextId = 0;
-    }
     $(task).prop('id', nextId);
     nextId++;
     localStorage.setItem("nextId", JSON.stringify(nextId));
@@ -14,16 +11,33 @@ function generateTaskId(task) {
 
 // Create a task card
 function createTaskCard(task) {
-    $(`#${task.todoStatus}-cards`).append(`<div class="card">
-    <div class="card-header bg-white">
-      <h2 class="card-title mb-1">${task.title}</h2>
+    const urgencyClasses = checkUrgency(task);
+    $(`#${task.todoStatus}`).append(`<div class="card ${urgencyClasses} todo-card" id="${task.id}">
+    <div class="card-header">
+    <h2 class="card-title mb-1">${task.title}</h2>
     </div>
-    <div class="card-body bg-light">
-      <div class="m-2">${task.description}</div>
-      <div class="m-2">${task.dueDate}</div>
-      <button type="button" class="btn btn-primary btn-danger">Delete</button>
+    <div class="card-body">
+    <div class="m-2">${task.description}</div>
+    <div class="m-2">${task.dueDate}</div>
+    <button type="button" class="btn btn-outline-light btn-danger task-delete-button">Delete</button>
     </div>
-  </div>`)
+    </div>`)
+}
+
+let checkUrgency = function(task) {
+    let urgencyClass;
+    const urgencyTime = dayjs(task.dueDate).diff(dayjs(), 'day') + 1;
+    if (task.todoStatus == 'done-cards') {
+        // urgencyClass = "bg-light"
+        return;
+    } else if (urgencyTime > 3) {
+        urgencyClass = "text-white bg-primary"
+    } else if (urgencyTime > 0) {
+        urgencyClass = "text-white bg-warning"
+    } else {
+        urgencyClass = "text-white bg-danger"
+    }
+    return urgencyClass;
 }
 
 // Render the task list
@@ -40,35 +54,62 @@ function handleAddTask(event){
         title: $('#formTitle').val(),
         dueDate: $('#formDate').val(),
         description: $('#formDescription').val(),
-        todoStatus: 'todo'
+        todoStatus: 'todo-cards'
     }
     $('.form-control').val('');
     generateTaskId(task);
     createTaskCard(task);
-    if (!taskList) {
-        taskList = [];
-    }
+    $( ".todo-card" ).draggable({
+        revert: "invalid",
+        connectToSortable: "#todo-cards, #in-progress-cards, #done-cards"
+    });
     taskList.push(task);
     localStorage.setItem("tasks", JSON.stringify(taskList));
 }
 
 //!!! Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
-
+    const delId = $(this).parent().parent().attr('id');
+    $(`#${delId}`).remove();
+    taskList = taskList.filter((task) => task.id != delId);
+    localStorage.setItem('tasks', JSON.stringify(taskList));
 }
 
 //!!! Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-
+    const currentStatus = this.id;
+    const cardTask = ui.draggable[0];
+    for (task of taskList) {
+        if (task.id == cardTask.id) {
+            task.todoStatus = currentStatus;
+            urgencyClasses = checkUrgency(task);
+            $(cardTask).removeClass('text-white bg-danger bg-warning bg-primary');
+            $(cardTask).addClass(urgencyClasses);
+        }
+    }
+    localStorage.setItem('tasks', JSON.stringify(taskList));
 }
 
 //!!! Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+    if (!taskList) {
+        taskList = [];
+    }
+    if (!nextId) {
+        nextId = 0;
+    }
     renderTaskList();
-    $( function() {
-        $( "#todo-cards, #in-progress-cards, #done-cards" ).sortable({
-            connectWith: ".listedTask"
-        }).disableSelection();
-    } );
+    $( ".todo-card" ).draggable({
+        revert: "invalid",
+        connectToSortable: "#todo-cards, #in-progress-cards, #done-cards"
+    });
+    $( "#todo-cards, #in-progress-cards, #done-cards" ).droppable();
+    $( "#todo-cards, #in-progress-cards, #done-cards" ).sortable();
+    $( "#formDate" ).datepicker({
+        changeMonth: true,
+        changeYear: true
+    });
     $('#addTask').on('submit', handleAddTask);
+    $('.task-delete-button').on('click', handleDeleteTask)
+    $('#todo-cards, #in-progress-cards, #done-cards').on("drop", handleDrop)
 });
